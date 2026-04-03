3411bfe06b294e2be5d2de97dcf3893849a9819a
Pedro Barbosa <pedrovbb89@gmail.com>
2026-04-03 18:31:46 -0500

    feat: add OpenCode config generator service

---
+++ app/services/opencode_config_generator.rb
@@ -0,0 +1,56 @@
+class OpencodeConfigGenerator
+  def initialize(mcp_url:, mcp_auth_token:, provider_keys: {})
+    @mcp_url = mcp_url
+    @mcp_auth_token = mcp_auth_token
+    @provider_keys = provider_keys
+  end
+
+  def generate
+    {
+      "provider" => build_providers,
+      "mcp" => build_mcp_config
+    }
+  end
+
+  def to_json
+    JSON.pretty_generate(generate)
+  end
+
+  def write_to(path)
+    FileUtils.mkdir_p(File.dirname(path))
+    File.write(path, to_json)
+  end
+
+  def self.from_settings
+    new(
+      mcp_url: "http://web:#{ENV.fetch('PORT', 3000)}/mcp",
+      mcp_auth_token: Setting.mcp_auth_token,
+      provider_keys: {
+        "anthropic" => ENV["ANTHROPIC_API_KEY"],
+        "openai" => ENV["OPENAI_API_KEY"]
+      }
+    )
+  end
+
+  private
+
+  attr_reader :mcp_url, :mcp_auth_token, :provider_keys
+
+  def build_providers
+    provider_keys
+      .select { |_name, key| key.present? }
+      .transform_values { |key| { "api_key" => key } }
+  end
+
+  def build_mcp_config
+    {
+      "maybe-finance" => {
+        "type" => "remote",
+        "url" => mcp_url,
+        "headers" => {
+          "Authorization" => "Bearer #{mcp_auth_token}"
+        }
+      }
+    }
+  end
+end
