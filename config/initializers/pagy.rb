3f5a63d9be986f5466fee5dd45f9a25fffa852f0
Pedro Barbosa <pedrovbb89@gmail.com>
2026-04-05 21:00:33 -0500

    fix: expose Pagy 43.x series method publicly and handle overflow redirect

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,1 +1,9 @@
 Pagy::OPTIONS[:limit] = 20
+
+# Pagy 43.x defines `series` as protected in the NumericHelpers module.
+# Our custom pagination view calls `pagy.series` directly, so we expose it publicly.
+require "pagy/toolbox/helpers/support/series"
+
+class Pagy
+  public :series
+end
